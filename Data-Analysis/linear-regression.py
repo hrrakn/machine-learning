a253bc071635f333ab11905914a48a762ea6cf99
ami.n <i.ami.nakagawa@globis.co.jp>
2019-12-24 00:03:15 +0900

    create linear-regression.py

---
+++ Data-Analysis/linear-regression.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+
+# データ読み込み
+df = pd.read_excel('[file name].xlsx',
+                   sheet_name='sheet name', encoding='utf-8')
+x = df[['explanatory variable']]
+y = df[['objective variable']]
+
+# 回帰線を作図
+model_lr = LinearRegression()
+model_lr.fit(x, y)
+
+plt.plot(x, y, 'o')
+plt.plot(x, model_lr.predict(x), linestyle="solid")
+plt.show()
+
+# 記述統計を表示
+x_add_const = sm.add_constant(x)
+model_sm = sm.OLS(y, x_add_const).fit()
+
+print('モデル関数の回帰変数 w1: %.3f' % model_lr.coef_)
+print('モデル関数の切片 w2: %.3f' % model_lr.intercept_)
+print('y= %.3fx + %.3f' % (model_lr.coef_, model_lr.intercept_))
+print('決定係数R^2:', model_lr.score(x, y))
+print(model_sm.summary())
